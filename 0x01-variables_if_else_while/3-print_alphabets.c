4f8c91d0a1b6c5113ae89723f2d6f5fb36ba2aa5
tamoer <armonchari@gmail.com>
2023-05-19 10:48:17 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a'; c <= 'z';  c++)
+	{
+		putchar(c);
+	}
+	for (C = 'A'; C <= 'Z'; C++)
+	{
+		putchar(C);
+	}
+	putchar('\n');
+	return (0);
+}
