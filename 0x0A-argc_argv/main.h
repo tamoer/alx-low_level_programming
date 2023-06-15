82137b293ed2585ff71bb6a0042316bb79e6fec0
tamoer <armonchari@gmail.com>
2023-06-14 17:30:10 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+int _atoi(char *s);
+int check_num(char *str);
+
+
+#endif
