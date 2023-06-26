3845bf7b85215972ba3ac8b24eca9d5b2b30f24c
tamoer <armonchari@gmail.com>
2023-06-26 03:34:41 -0700

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of a file it was compiled from
+ * Return: always 0 (Success)
+ */
+
+int main (void)
+{
+	char filename[] = __FILE__;
+	int i = 0;
+
+	while (filename[i] != '\0')
+	{
+		putchar(filename[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+
+}
